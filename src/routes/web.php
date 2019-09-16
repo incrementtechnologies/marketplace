5edbd8a01ef5549ecd08c63978819c282e32c89e
kennCK <kennettecanales@gmail.com>
2019-09-16 14:18:12 +0800

    udpate

--- src/routes/web.php
+++ src/routes/web.php
@@ -32,6 +32,7 @@ $route = env('PACKAGE_ROUTE', '').'/merchants/';
 $controller = 'Increment\Marketplace\Http\MerchantController@';
 Route::post($route.'create', $controller."create");
 Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'retrieve_all', $controller."retrieveAll");
 Route::post($route.'update', $controller."update");
 Route::post($route.'delete', $controller."delete");
 Route::get($route.'test', $controller."test");
