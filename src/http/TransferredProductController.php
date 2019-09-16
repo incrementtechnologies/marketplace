6d0e31f708fbcd5aec8871a960b2c189e56682c5
kennCK <kennettecanales@gmail.com>
2019-09-16 18:40:59 +0800

    add job

--- src/http/TransferredProductController.php
+++ src/http/TransferredProductController.php
@@ -6,9 +6,17 @@ namespace Increment\Marketplace\Http;
 use Illuminate\Http\Request;
 use App\Http\Controllers\APIController;
 use Increment\Marketplace\Models\TransferredProduct;
+use Increment\Marketplace\Jobs\TransferredProduct as TransferredProductJob;
 class TransferredProductController extends APIController
 {
     function __construct(){
       $this->model = new TransferredProduct();
+    }
+
+    public function create(Request $request){
+      $data = $request->all();
+      TransferredProductJob::dispatch($data['products'], $data['transfer_id']);
+      $this->response['data'] = true;
+      return $this->response();
     }
 }
