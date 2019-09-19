84d6f4e6ae28e83ae3b07c15e560ee0eb0c81be8
kennCK <kennettecanales@gmail.com>
2019-09-19 21:09:58 +0800

    update

--- src/http/TransferredProductController.php
+++ src/http/TransferredProductController.php
@@ -6,7 +6,7 @@ namespace Increment\Marketplace\Http;
 use Illuminate\Http\Request;
 use App\Http\Controllers\APIController;
 use Increment\Marketplace\Models\TransferredProduct;
-use Increment\Marketplace\Jobs\TransferredProduct as TransferredProductJob;
+use Carbon\Carbon;
 class TransferredProductController extends APIController
 {
     function __construct(){
@@ -15,8 +15,21 @@ class TransferredProductController extends APIController
 
     public function create(Request $request){
       $data = $request->all();
-      TransferredProductJob::dispatch($data['products'], $data['transfer_id']);
-      $this->response['data'] = true;
+      if(sizeof($data['products']) > 0){
+        $array = array();
+        for ($i=0; $i < sizeof($data['products']) - 1; $i++) {
+          $array[] = array(
+            'transfer_id' => $data['transfer_id'],
+            'payload'     => 'product_traces',
+            'payload_value' => $data['products'][$i]['id'],
+            'created_at'    => Carbon::now()
+          );
+        }
+        TransferredProduct::insert($array);
+        $this->response['data'] = true;
+      }else{
+        $this->response['data'] = false;
+      }
       return $this->response();
     }
 
