4a5f50b12603112c0afb62bd933bf1abd46f1e9a
kennCK <kennettecanales@gmail.com>
2019-12-13 17:04:31 +0800

    update

--- src/http/TransferredProductController.php
+++ src/http/TransferredProductController.php
@@ -68,7 +68,7 @@ class TransferredProductController extends APIController
     }
 
     public function getSize($column, $value, $date){
-      $result = TransferredProduct::where($column, '=', $value)->where('created_at', '>=', $date)->count();
+      $result = TransferredProduct::where($column, '=', $value)->where('created_at', '>=', $date)->where('deleted_at', '=', null)->count();
       return $result;
     }
 
