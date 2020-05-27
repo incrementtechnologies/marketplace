37009db5f0e546509d2e6e04ce179907d537bd27
kennCK <kennettecanales@gmail.com>
2020-05-27 21:04:05 +0800

    update

--- src/http/TransferController.php
+++ src/http/TransferController.php
@@ -151,8 +151,7 @@ class TransferController extends APIController
           $product = null;
           $index = null;
           $index = array_search(intval($key), array_column($this->response['data'], 'id'), true);
-          echo $index.'/';
-          if($index && $index != false){
+          if($index >= 0){
             $this->response['data'][$index]['qty_in_bundled'] += sizeof($value);
           }else{
             $product =  app($this->productClass)->getProductByParams('id', intval($key));
