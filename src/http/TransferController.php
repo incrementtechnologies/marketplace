aee92844891d57ad0d97e16da47aae092b483dba
kennCK <kennettecanales@gmail.com>
2020-05-27 19:32:44 +0800

    update

--- src/http/TransferController.php
+++ src/http/TransferController.php
@@ -148,7 +148,7 @@ class TransferController extends APIController
         $bundled = $bundled->groupBy('product_on_settings');
         foreach ($bundled as $key => $value) {
           $index = array_search(intval($key), array_column($this->response['data'], 'id'));
-          if(!$index || $index != false){
+          if(!$index && $index != false){
             $this->response['data'][$index]['qty_in_bundled'] += sizeof($value);
           }else{
             $product =  app($this->productClass)->getProductByParams('id', intval($key));
