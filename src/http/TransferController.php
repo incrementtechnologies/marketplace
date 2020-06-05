b9d85d20d2f9d13db6648839f62c053431b12b09
kennCK <kennettecanales@gmail.com>
2020-06-05 10:13:48 +0800

    update

--- src/http/TransferController.php
+++ src/http/TransferController.php
@@ -101,14 +101,14 @@ class TransferController extends APIController
       $data = $request->all();
       $result = DB::table('transfers as T1')
       ->join('transferred_products as T2', 'T2.transfer_id', '=', 'T1.id')
-      ->join('products as T3', function($join){
-          $join->on('T3.id', '=', 'T2.product_id');
-          // if(isset($data['type']) && $data['type'] != 'regular'){
-          //   $join->on('T3.id', '=', 'T2.product_id')->where('T3.type', '=', 'bundled')->orWhere('T3.type', '=', 'custom_bundled');
-          // }else{
-          //   $join->on('T3.id', '=', 'T2.product_id')->where('T3.type', '=', 'regular');
-          // }
-       })
+      // ->join('products as T3', function($join){
+      //     $join->on('T3.id', '=', 'T2.product_id');
+      //     // if(isset($data['type']) && $data['type'] != 'regular'){
+      //     //   $join->on('T3.id', '=', 'T2.product_id')->where('T3.type', '=', 'bundled')->orWhere('T3.type', '=', 'custom_bundled');
+      //     // }else{
+      //     //   $join->on('T3.id', '=', 'T2.product_id')->where('T3.type', '=', 'regular');
+      //     // }
+      //  })
       ->where('T1.to', '=', $data['merchant_id'])
       ->where('T2.deleted_at', '=', null)
       ->where('T1.deleted_at', '=', null)
