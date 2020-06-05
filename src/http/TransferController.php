50f255ed7ba97303687cefc5c9a8437dfa262fe2
kennCK <kennettecanales@gmail.com>
2020-06-05 15:36:45 +0800

    update

--- src/http/TransferController.php
+++ src/http/TransferController.php
@@ -104,6 +104,7 @@ class TransferController extends APIController
       ->where('T1.to', '=', $data['merchant_id'])
       ->where('T2.deleted_at', '=', null)
       ->where('T1.deleted_at', '=', null)
+      ->limit(10)
       ->get(['T2.product_id', 'T2.created_at', 'T2.payload_value']);
 
       $result = $result->groupBy('product_id');
