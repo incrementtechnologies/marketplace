f3a028fcb0e0dddfb7b9bd6cea26f40c969254b6
kennCK <kennettecanales@gmail.com>
2020-08-03 21:24:51 +0800

    update

--- src/migrations/2019_31_03_100038_create_order_requests_table.php
+++ src/migrations/2019_31_03_100038_create_order_requests_table.php
@@ -19,7 +19,6 @@ class CreateOrderRequestsTable extends Migration
             $table->bigInteger('account_id');
             $table->bigInteger('merchant_id');
             $table->bigInteger('merchant_to');
-            $table->bigInteger('delivered_by');
             $table->dateTime('date_of_delivery');
             $table->dateTime('date_delivered')->nullable();
             $table->bigInteger('delivered_by')->nullable();
