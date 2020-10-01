fb0255c0754ae8ad04b9f450a5ab5c617f6ab0f8
capstone student <capstonestudent@pn.local>
2020-10-01 12:09:36 +0800

    level search

--- src/http/OrderRequestController.php
+++ src/http/OrderRequestController.php
@@ -5,6 +5,7 @@ namespace Increment\Marketplace\Http;
 use Illuminate\Http\Request;
 use App\Http\Controllers\APIController;
 use Increment\Marketplace\Models\OrderRequest;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 class OrderRequestController extends APIController
 {
@@ -74,6 +75,85 @@ class OrderRequestController extends APIController
       $array[] = $item;
     }
     return $array;
+  }
+
+  public function retrieveSecondLevel(Request $request){
+    $data = $request->all();
+    $con = $data['condition'];
+    // dd($data['sort']['name']);
+      // dd($data);
+    // $result = DB::table('order_requests')
+    // ->join('merchants','order_requests.merchant_to','=','merchants.id')
+    // ->join('accounts', 'accounts.id', '=', 'order_requests.delivered_by')
+    // ->where($con[1]['column'], $con[1]['clause'], $con[1]['value'])
+    // ->whereNull('order_requests.delivered_by')
+    // ->select('order_requests.*', 'merchants.name')->skip($data['offset'])->take($data['limit'])->get();
+    if($con[1]['column'] === 'name'){
+    $result = DB::table('order_requests')
+      ->join('merchants','order_requests.merchant_to','=','merchants.id')
+      ->leftJoin('accounts', 'order_requests.delivered_by', '=', 'accounts.id')
+      ->Where($con[0]['column'], $con[0]['clause'], $con[0]['value'])
+      ->where($con[1]['column'], $con[1]['clause'], $con[1]['value'])
+      ->select('order_requests.*', 'merchants.name')->skip($data['offset'])->take($data['limit'])
+      ->orderBy('name',$data['sort']['name'])->get();
+    }else if($con[1]['column'] === 'username' && $con[1]['value'] === '%%'){
+      $result = DB::table('order_requests')
+      ->join('merchants','order_requests.merchant_to','=','merchants.id')
+      ->leftJoin('accounts', 'order_requests.delivered_by', '=', 'accounts.id')
+      ->Where($con[0]['column'], $con[0]['clause'], $con[0]['value'])
+      ->select('order_requests.*', 'merchants.name')->skip($data['offset'])->take($data['limit'])
+      ->orderBy('username',$data['sort']['username'])->get();
+    }else{
+      $string = $con[1]['value'];
+      $fields = ['username','first_name', 'last_name'];
+      $result = DB::table('order_requests')
+      ->join('merchants','order_requests.merchant_to','=','merchants.id')
+      ->leftJoin('accounts', 'order_requests.delivered_by', '=', 'accounts.id')
+      ->leftJoin('account_informations', 'account_informations.account_id', '=', 'accounts.id')
+      ->Where($con[0]['column'], $con[0]['clause'], $con[0]['value'])
+      ->where(function($query) use ($fields, $string){
+            for($i = 0; $i < count($fields); $i++){
+              $query->orWhere($fields[$i], 'like', $string);
+            }
+      })
+      ->select('order_requests.*', 'merchants.name')->skip($data['offset'])->take($data['limit'])
+      ->orderBy('username',$data['sort']['username'])->get();
+    }
+      if(sizeof($request) > 0){
+        $this->response['data'] =  $this->manageLevelResult($result);
+      }
+    $this->response['size'] = OrderRequest::where($data['condition'][0]['column'], '=', $data['condition'][0]['value'])->count();
+    return $this->response();
+  }
+  public function manageLevelResult($result){
+    $array = array();
+    foreach ($result as $key) {
+      $item = array(
+        'merchant_to' => $key->name,
+        'date_of_delivery'  => Carbon::createFromFormat('Y-m-d H:i:s', $key->date_of_delivery)->copy()->tz($this->response['timezone'])->format('F j, Y'),
+        'status'        => $key->status,
+        'delivered_by'  => $key->delivered_by ? $this->retrieveName($key->delivered_by) : null,
+        'delivered_date'=> $key->date_delivered ? Carbon::createFromFormat('Y-m-d H:i:s', $key->date_delivered)->copy()->tz($this->response['timezone'])->format('F j, Y H:i:s') : null,
+        'code'          => $key->code,
+        'added_by'      => $key->code,
+        'id'      => $key->id,
+        'order_number'      => $key->order_number,
+        'daily_loading_list' => app($this->dailyLoadingListClass)->checkIfExist('order_request_id', $key->id)
+      );
+      $array[] = $item;
+    }
+    return $array;
+  }
+
+  public function retreiveDeliveredBy($id){
+    if($id != null){
+      $result = DB::table('accounts as T1')
+      ->where('T1.id', '=', $id)
+      ->select('T2.*')->get()->first();
+      return $result->name != null ? $result->name : $result->username;
+    }else{
+      return null;
+    }    
   }
 
   public function updateByParams($id, $array){
