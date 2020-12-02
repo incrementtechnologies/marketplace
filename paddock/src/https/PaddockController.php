02b9bf35cb414165e8898b0e7d31a1dc92b899de
ecvelayo <ecvelayo@gmail.com>
2020-12-02 15:36:32 +0800

    retrieve additional info on paddocks c/o Herrera

--- paddock/src/https/PaddockController.php
+++ paddock/src/https/PaddockController.php
@@ -5,6 +5,7 @@ namespace Increment\Marketplace\Paddock\Http;
 use Illuminate\Http\Request;
 use App\Http\Controllers\APIController;
 use Increment\Marketplace\Paddock\Models\Paddock;
+use Increment\Marketplace\Paddock\Models\PaddockPlan;
 use Carbon\Carbon;
 
 class PaddockController extends APIController
@@ -16,4 +17,17 @@ class PaddockController extends APIController
             'note'
         );
     }    
+
+    public function retrieve(Request $request){
+        $data = $request->all();
+        $this->model = new Paddock();
+        $this->retrieveDB($data);
+        for ($i = 0; $i < count($this->response['data']); $i++){
+            $paddockData = $this->response['data'];
+            $paddock_id = $this->response['data']['0']['id'];
+            $paddock_data = PaddockPlan::select()->where('paddock_id', '=', $paddock_id)->orderBy('created_at','desc')->limit(2)->get();
+            $this->response['data'][$i]['paddock_data'] = $paddock_data;
+        }
+        return $this->response();
+    }
 }
