afecbf08e0529f6069f1826d2986fa6966085243
kennck <kennettecanales@gmail.com>
2021-01-08 15:19:23 +0800

    update

--- paddock/src/http/SprayMixController.php
+++ paddock/src/http/SprayMixController.php
@@ -133,6 +133,7 @@ class SprayMixController extends APIController
 
     public function getByParamsDefault($column, $value){
         $result = SprayMix::where($column, '=', $value)->get();
+        echo json_encode($result);
         return sizeof($result) > 0 ? $result[0] : null;
     }
 }
