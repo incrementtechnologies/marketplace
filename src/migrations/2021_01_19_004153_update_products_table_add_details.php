5bcd136afe7e9634695a47fd3100218e0e23281b
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-01-19 13:35:55 +0800

    miggration

--- src/migrations/2021_01_19_004153_update_products_table_add_details.php
+++ src/migrations/2021_01_19_004153_update_products_table_add_details.php
@@ -14,7 +14,7 @@ class UpdateProductsTableAddDetails extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('details')->after('status');
+            $table->string('details')->after('status')->default(json_encode(["{solvent: '', safety: '', formulation: '', group: '', active: [''], safety_equipment: [''], mixing_order: [''], files: [{url: '', title: ''}, {url: '', title: ''}]}"]));
         });
     }
 
