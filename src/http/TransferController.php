7d0a28746a53e95d4434e3b5b5976488c9ddc734
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-02-22 13:40:22 +0800

    with dd for test

--- src/http/TransferController.php
+++ src/http/TransferController.php
@@ -1098,6 +1098,7 @@ class TransferController extends APIController
     $i = 1;
     $size = $result->count();
     $testArray = array();
+    dd($result);
     // $this->response['data'] = $result;
     // return $this->response();
     if(sizeof($result) > 0){ 
