de871baf6f62765453045c5e09a1dfc5caed13a5
= <=>
2021-03-11 20:09:50 +0800

    update

--- src/http/ProductController.php
+++ src/http/ProductController.php
@@ -79,13 +79,24 @@ class ProductController extends APIController
       $con = $data['condition'];
       $inventoryType = $data['inventory_type'];
       $accountId = $data['account_id'];
-      $result = Product::where($con[0]['column'], $con[0]['clause'], $con[0]['value'])
-          ->where($con[1]['column'], $con[1]['clause'], $con[1]['value'])
-          ->where('type', '!=', 'bundled')
-          ->orderBy(array_keys($data['sort'])[0], $data['sort'][array_keys($data['sort'])[0]])
-          ->skip($data['offset'])
-          ->take($data['limit'])
-          ->get();
+      $result = null;
+      if(sizeof($con) == 1){
+        $result = Product::where($con[0]['column'], $con[0]['clause'], $con[0]['value'])
+            ->where('type', '!=', 'bundled')
+            ->orderBy(array_keys($data['sort'])[0], $data['sort'][array_keys($data['sort'])[0]])
+            ->skip($data['offset'])
+            ->take($data['limit'])
+            ->get();  
+      }else if(sizeof($con) == 2){
+        $result = Product::where($con[0]['column'], $con[0]['clause'], $con[0]['value'])
+            ->where($con[1]['column'], $con[1]['clause'], $con[1]['value'])
+            ->where('type', '!=', 'bundled')
+            ->orderBy(array_keys($data['sort'])[0], $data['sort'][array_keys($data['sort'])[0]])
+            ->skip($data['offset'])
+            ->take($data['limit'])
+            ->get();        
+      }
+
       $this->response['data'] = $result;
       // $this->model = new Product();
       // $this->retrieveDB($data);
