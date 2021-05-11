5f213e8f11993797ec1bf5c339d33b7c9a7875e4
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-05-11 16:32:52 +0800

    some changes

--- src/http/BundledSettingController.php
+++ src/http/BundledSettingController.php
@@ -149,7 +149,15 @@ class BundledSettingController extends APIController
       $i = 0;
       foreach ($result as $key) {
         $traceQty = app($this->productTraceController)->getProductQtyByParams($result[$i]['bundled'], $result[$i]['product_attribute_id']);
+        $parentTrace = app($this->productTraceController)->getTraceByParams(
+            array(
+              array('product_id', '=', $result[$i]['product_id']),
+              array('product_attribute_id', '=', $result[$i]['product_attribute_id']),
+            ),
+            ['id', 'product_attribute_id', 'product_id']
+        );
         // $result[$i]['product'] = app($this->productController)->getByParamsWithReturn('id', $result[$i]['product_id'], ['title', 'id', 'tags']);
+        $result[$i]['parent_trace'] = $parentTrace;
         $result[$i]['variation'] = app($this->productAttrController)->getByParamsWithMerchant('id', $result[$i]['product_attribute_id'], $merchantId);
         $result[$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz($this->response['timezone'])->format('F j, Y H:i A');
         $result[$i]['qty'] = (int)$result[$i]['qty'];
