5fea4551b7d1f48afc126c5665745a6fe62e6b2e
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-09-24 13:48:34 +0800

    bundled qty

--- src/http/BundledSettingController.php
+++ src/http/BundledSettingController.php
@@ -142,7 +142,7 @@ class BundledSettingController extends APIController
         $result[$i]['variation'] = app($this->productAttrController)->getByParamsWithMerchant('id', $result[$i]['product_attribute_id'], $merchantId);
         $result[$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz($this->response['timezone'])->format('F j, Y H:i A');
         $result[$i]['qty'] = (int)$result[$i]['qty'];
-        $result[$i]['scanned_qty'] = $traceQty - $totalTransferredBundled;
+        $result[$i]['scanned_qty'] = $traceQty - $totalTransferredBundled[0]['bundled_setting_qty'];
         $result[$i]['is_transferred'] = false;
         if(sizeof($bundledProduct) > 0){
           array_push($res, $result[$i]);
@@ -177,7 +177,7 @@ class BundledSettingController extends APIController
         $result[$i]['variation'] = app($this->productAttrController)->getByParamsWithMerchant('id', $result[$i]['product_attribute_id'], $merchantId);
         $result[$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz($this->response['timezone'])->format('F j, Y H:i A');
         $result[$i]['qty'] = (int)$result[$i]['qty'];
-        $result[$i]['scanned_qty'] =  $traceQty - $totalTransferredBundled;
+        $result[$i]['scanned_qty'] =  $traceQty - $totalTransferredBundled[0]['bundled_setting_qty'];
         $result[$i]['product'] = app($this->productController)->getProductColumnWithReturns('id', $result[$i]['bundled'], ['title']);
         $result[$i]['component_product'] = app($this->productController)->getProductColumnWithReturns('id', $result[$i]['product_id'], ['title']);
         $result[$i]['component_qty'] = app($this->productTraceController)->getProductQtyByParams($result[$i]['product_id'], $result[$i]['product_attribute_id']);
