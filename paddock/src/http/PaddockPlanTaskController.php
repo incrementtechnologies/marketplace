ce57ef744d012279cde6855ab35f3aa6c50737e5
pathran-phantom1823 <patrick.pathran23@gmail.com>
2022-05-26 09:10:30 +0800

    updates

--- paddock/src/http/PaddockPlanTaskController.php
+++ paddock/src/http/PaddockPlanTaskController.php
@@ -372,12 +372,16 @@ class PaddockPlanTaskController extends APIController
 
     public function retrievePaddockPlanTaskByParamsCompleted($column, $column2, $value)
     {
-        $batch = DB::table('batches as T1')
-            ->rightJoin('batch_paddock_tasks as T2', 'T1.id', '=', 'T2.batch_id')
-            ->where('T1.' . $column, '=', $value)
-            ->where('status', '=', 'completed')
-            ->where('T1.deleted_at', '=', null)
-            ->orderBy('T1.created_at', 'desc')->get()->toArray();
+        // $batch = DB::table('batches as T1')
+        //     ->rightJoin('batch_paddock_tasks as T2', 'T1.id', '=', 'T2.batch_id')
+        //     ->where('T1.' . $column, '=', $value)
+        //     ->where('status', '=', 'completed')
+        //     ->where('T1.deleted_at', '=', null)
+        //     ->orderBy('T1.created_at', 'desc')->get()->toArray();
+        $batch = Batch::where($column, '=', $value)
+        ->where('status', '=', 'completed')
+        ->where('deleted_at', '=', null)
+        ->orderBy('created_at', 'desc')->get()->toArray();
         $orders = OrderRequest::where($column, '=', $value)->orWhere($column2, '=', $value)->where('status', '=', 'completed')->orderBy('created_at', 'desc')->get();
         $orderArray = app($this->orderRequestClass)->manageResultsMobile($orders);
         $obj = array_merge($batch, $orderArray);
@@ -387,18 +391,25 @@ class PaddockPlanTaskController extends APIController
             $array = json_decode(json_encode($obj), true);
             foreach ($array as $key) {
                 if (!isset($array[$i]['code'])) {
-                    $paddockId = $this->retrieveByParams('id', $array[$i]['paddock_plan_task_id'], 'paddock_id');
-                    $array[$i]['paddock'] = $paddockId != null ? app($this->paddockClass)->getByParams('id', $paddockId, ['id', 'name']) : null;
-                    if($array[$i]['paddock'] != null) {
+                    if(isset($array[$i]['paddock_plan_task_id'])){
+                        $paddockId = $this->retrieveByParams('id', $array[$i]['paddock_plan_task_id'], 'paddock_id');
+                        $array[$i]['paddock'] = $paddockId != null ? app($this->paddockClass)->getByParams('id', $paddockId, ['id', 'name']) : null;
+                        if($array[$i]['paddock'] != null) {
+                            $array[$i]['date_completed_orig'] = $key['updated_at'];
+                            $array[$i]['date_completed'] = isset($key['updated_at']) ? Carbon::createFromFormat('Y-m-d H:i:s', $key['updated_at'])->copy()->tz($this->response['timezone'])->format('d M') : null;
+                            $array[$i]['nickname'] = $this->retrieveByParams('id', $array[$i]['paddock_plan_task_id'], 'nickname');
+                            $array[$i]['paddock_id'] = $this->retrieveByParams('id', $array[$i]['paddock_plan_task_id'], 'paddock_id');
+                            $array[$i]['spray_mix_id'] = $this->retrieveByParams('id', $array[$i]['paddock_plan_task_id'], 'spray_mix_id');
+                            $array[$i]['spray_mix'] = app($this->sprayMixClass)->getByParams('id', $array[$i]['spray_mix_id'], ['id', 'name']);
+                            $array[$i]['machine'] = app($this->machineClass)->getMachineNameByParams('id', $array[$i]['machine_id']);
+                            $array[$i]['recent_event'] = 'Task';
+                        }
+                    }else{
+                        $array[$i]['recent_event'] = 'Manual Batch';
                         $array[$i]['date_completed_orig'] = $key['updated_at'];
-                        $array[$i]['date_completed'] = isset($key['updated_at']) ? Carbon::createFromFormat('Y-m-d H:i:s', $key['updated_at'])->copy()->tz($this->response['timezone'])->format('d M') : null;
-                        $array[$i]['nickname'] = $this->retrieveByParams('id', $array[$i]['paddock_plan_task_id'], 'nickname');
-                        $array[$i]['paddock_id'] = $this->retrieveByParams('id', $array[$i]['paddock_plan_task_id'], 'paddock_id');
-                        $array[$i]['spray_mix_id'] = $this->retrieveByParams('id', $array[$i]['paddock_plan_task_id'], 'spray_mix_id');
-                        $array[$i]['spray_mix'] = app($this->sprayMixClass)->getByParams('id', $array[$i]['spray_mix_id'], ['id', 'name']);
-                        $array[$i]['machine'] = app($this->machineClass)->getMachineNameByParams('id', $array[$i]['machine_id']);
                     }
                 }else{
+                    $array[$i]['recent_event'] = 'Order';
                     $array[$i]['date_completed'] = $key['delivered_date_formatted'];
                     $array[$i]['date_completed_orig'] = $key['updated_at'];
                 }
